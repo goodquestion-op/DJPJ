380b81b812ee2fca1d306c9502e5dbfa6fb05613
Ash Swainson <0348422@schoolsnet.act.edu.au>
2026-02-27 11:13:27 +1100

    models

--- DJPJ/MyApp1/models.py
+++ DJPJ/MyApp1/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class teacher (models.model):
+class teacher (models.Model):
     Name = models.CharField(max_length=25)
+    Area = models.CharField(max_length=30)
 
 # Create your models here.
