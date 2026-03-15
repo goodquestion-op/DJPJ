38030c00a87d436fb2b80d7eb634431975eb625e
Ash Swainson <0348422@schoolsnet.act.edu.au>
2026-03-16 10:26:19 +1100

    monday of last week looking at making a drop down menu

--- DJPJ/MyApp1/forms.py
+++ DJPJ/MyApp1/forms.py
@@ -1,5 +1,18 @@
+from dataclasses import field
 from django import forms
 from .models import teacher
+
+class CarForm(forms.ModelForm):
+    class Meta:
+        model = teacher
+        fields = ['Name','Area']
+    def __int__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['Name'].widget.attrs.update({"class": "form-control"})
+        for fields in self.fields:
+            self.fields[field].widget.attrs.update({'class':"form-control"})
+
+        
 
 class InputForm(forms.ModelForm):
     class Meta:
