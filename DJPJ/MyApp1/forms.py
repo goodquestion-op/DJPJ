2efc9ff9d08a05044c42d6330170b8f85c038b2e
Ash Swainson <0348422@schoolsnet.act.edu.au>
2026-03-18 14:57:37 +1100

    IT'S STILL NOT MAKING SENSE

--- DJPJ/MyApp1/forms.py
+++ DJPJ/MyApp1/forms.py
@@ -2,10 +2,19 @@ from dataclasses import field
 from django import forms
 from .models import teacher
 
+FRUIT_CHOICES= [
+    ('orange', 'Oranges'),
+    ('cantaloupe', 'Cantaloupes'),
+    ('mango', 'Mangoes'),
+    ('honeydew', 'Honeydews'),
+    ]
+
 class CarForm(forms.ModelForm):
     class Meta:
         model = teacher
         fields = ['Name','Area']
+
+
     def __int__(self, *args, **kwargs):
         super().__init__(*args,**kwargs)
         self.fields['Name'].widget.attrs.update({"class": "form-control"})
@@ -18,5 +27,20 @@ class InputForm(forms.ModelForm):
     class Meta:
         model = teacher
         fields = ['Name', 'Area']
+
+ 
+
+class UserForm(forms.Form):
+    first_name= forms.CharField(max_length=100)
+    last_name= forms.CharField(max_length=100)
+    email= forms.EmailField()
+    age= forms.IntegerField()
+    favorite_fruit= forms.CharField(label='What is your favorite fruit?', widget=forms.Select(choices=FRUIT_CHOICES))
+
+class test(forms.Form):
+    class Meta:
+        model = teacher
+        fields = ['Name']
+
 
 
