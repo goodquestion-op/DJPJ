9cbd5e8ae7c47e3f767beed1d9075392b6de855e
Ash Swainson <0348422@schoolsnet.act.edu.au>
2026-04-01 14:17:56 +1100

    I had it as not the right html file to recive the request from

--- DJPJ/MyApp1/views.py
+++ DJPJ/MyApp1/views.py
@@ -13,10 +13,29 @@ def index(request):
 
   return render(request, "MyApp1/index.html",{'content': teach})
 
-def my_view(request):
+def index(request):
     form = Mydropdownform()
     return render(request, "MyApp1/index.html", {'form': form})
-  
+
+
+ 
+def some_view(request):
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+
+    # Create the PDF object, using the response object as its "file."
+    p = canvas.Canvas(response)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(100, 100, "Hello world.")
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+    return response
+
 def about(request):
    return render(
       request,
