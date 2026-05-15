119edfca7c59d401f5cdc72815d978804d9a1fd4
Ash Swainson <0348422@schoolsnet.act.edu.au>
2026-05-15 12:02:42 +1000

    many deletes this time

--- DJPJ/MyApp1/models.py
+++ DJPJ/MyApp1/models.py
@@ -5,41 +5,31 @@ from django.db import models
 
 
 
-#class Areas (models.Model):
-#        Area = models.CharField(max_length=25)
-#        def __str__(self):
-#           return f"{self.Area} "
+class Areas (models.Model):
+        Area = models.CharField(max_length=25)
+        def __str__(self):
+           return f"{self.Area} "
 
-#class teacher (models.Model):
+class teacher (models.Model):
 
-#   Name = models.CharField(max_length=25)
-#   Area = models.ManyToManyField(Areas)
-#   def __str__(self):
-#        return f"{self.Name} "
-  
-   
+   Name = models.CharField(max_length=25)
+   Area = models.ManyToManyField(Areas)
+   def __str__(self):
+        return f"{self.Name} "
+        
+class Courses (models.Model):
+       Course = models.CharField(max_length=100)
+       Area = models.ForeignKey(Areas, on_delete=models.CASCADE)
 
-    
+       def __str__(self):
+            return f"{self.Course} "
 
-#class Courses (models.Model):
-#       Course = models.CharField(max_length=100)
-#       Area = models.ManyToManyField(Areas)
+class Units (models.Model):
+   Area = models.ForeignKey(Areas, on_delete=models.CASCADE)
+   Unit = models.CharField(max_length=30)
 
-#       def __str__(self):
-#            return f"{self.Course} "
-
-
-
-    
-      
-   
-
-#class Units (models.Model):
-#   Area = models.ForeignKey(Areas, on_delete=models.CASCADE)
-#   Unit = models.CharField(max_length=30)
-
-#   def __str__(self):
-#            return f"{self.Unit} "
+   def __str__(self):
+            return f"{self.Unit} "
 
 
 
