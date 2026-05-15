0370e0e7751bbed06f5b1a30be933169d5a813b2
Ash Swainson <0348422@schoolsnet.act.edu.au>
2026-05-15 11:52:18 +1000

    wiping everything

--- DJPJ/MyApp1/models.py
+++ DJPJ/MyApp1/models.py
@@ -1,33 +1,36 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class CustomUser(User):
-    phone_number = models.CharField(max_length=15, blank=True)
 
 
 
-class Areas (models.Model):
-        Area = models.CharField(max_length=25)
-        def __str__(self):
-           return f"{self.Area} "
+#class CustomUser(User):
+#    phone_number = models.CharField(max_length=15, blank=True)
 
-class teacher (models.Model):
 
-   Name = models.CharField(max_length=25)
-   Area = models.ManyToManyField(Areas)
-   def __str__(self):
-        return f"{self.Name} "
+
+#class Areas (models.Model):
+#        Area = models.CharField(max_length=25)
+#        def __str__(self):
+#           return f"{self.Area} "
+
+#class teacher (models.Model):
+
+#   Name = models.CharField(max_length=25)
+#   Area = models.ManyToManyField(Areas)
+#   def __str__(self):
+#        return f"{self.Name} "
   
    
 
     
 
-class Courses (models.Model):
-       Course = models.CharField(max_length=100)
+#class Courses (models.Model):
+#       Course = models.CharField(max_length=100)
+#       Area = models.ManyToManyField(Areas)
 
-
-       def __str__(self):
-            return f"{self.Course} "
+#       def __str__(self):
+#            return f"{self.Course} "
 
 
 
@@ -35,9 +38,12 @@ class Courses (models.Model):
       
    
 
-class Units (models.Model):
-   Area = models.ForeignKey(Areas, on_delete=models.CASCADE)
-   Unit = models.CharField(max_length=30)
+#class Units (models.Model):
+#   Area = models.ForeignKey(Areas, on_delete=models.CASCADE)
+#   Unit = models.CharField(max_length=30)
+
+#   def __str__(self):
+#            return f"{self.Unit} "
 
 
 
